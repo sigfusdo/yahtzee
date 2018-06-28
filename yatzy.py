995e2a1a4d977d5b3ed7dec794726188d6265e6c
sigfusdo <gudrunsi@student.matnat.uio.no>
2018-06-28 23:17:20 +0200

    Endret yatzy

--- yatzy.py
+++ yatzy.py
@@ -1,9 +1,9 @@
 import random
 
 # Lag en variabel som du kaller antall_sider. Be brukeren velge antall sider ved å bruke input()-funksjonen.
-antall_sider = 6
+
 ### Her gjør vi om antall_sider fra å være en teksstreng til å være et heltall (fra sting til int)
-#antall_sider = int(antall_sider)
+antall_sider = int(antall_sider)
 
 
 def ikke_yahtzee():
@@ -15,10 +15,11 @@ def ikke_yahtzee():
     return False
 
 # Lag en variabel antall_kast, som er lik 0
-antall_kast = 0
+
+
 while ikke_yahtzee():
     # Øk variabelen antall_kast med 1
-    antall_kast += 1
+    
 
 print("Det tok " + str(antall_kast) + " kast før du fikk yatzy.")
 print("Teoretisk er sannsynligheten for yahtzee " + str((1/antall_sider)**5))
